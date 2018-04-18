1ef01267bbd4c5aeab034657c0411a9a23faa711
Benjamin Pisano <benjamin.pisano@icloud.com>
2018-04-18 19:26:57 +0200

    Initial Commit

---
+++ light_shot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  light_shot
+//
+//  Created by Benjamin Pisano on 18/04/2018.
+//  Copyright © 2018 Snopia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
